f69dd0c5910b7243fe200a5cb54d2e7a7435009d
Abhishek <abhishekparmar162@gmail.com>
2022-09-19 14:08:03 +0530

    started a new project

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1 className='text-red-600 text-4xl cursor-pointer font-bold '>NETFLIX</h1>
+        <button>Sign IN</button>
+        <button>Sign Up</button>
+    </div>
+  )
+}
+
+export default NavBar
